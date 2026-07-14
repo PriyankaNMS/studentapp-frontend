e540cf5ec85c89e812451cb0969a3c1cf647278d
priyanka <kpriyanka@nmsworks.co.in>
2026-07-14 12:14:00 +0530

    Left margin is added to form input fields.

--- src/components/StudentForm.js
+++ src/components/StudentForm.js
@@ -29,16 +29,16 @@ function StudentForm({ initialData, onSubmit, submitLabel }) {
             boxShadow: "2px 2px 10px lightgrey"
         }}>
             <label htmlFor="name" >Name</label>
-            <input name="name" placeholder="Your_name" value={form.name} onChange={handleChange} required style={{height:"35px",width:'510px',margin:"10px",backgroundColor:"snow"}}/><br/>
+            <input name="name" placeholder="Your_name" value={form.name} onChange={handleChange} required style={{height:"35px",width:'400px',marginLeft:"20px",margin:"10px",paddingLeft:"5px",backgroundColor:"snow"}}/><br/>
 
             <label htmlFor="email">Email</label>
-            <input name="email" placeholder="mail@gmail.com" value={form.email} onChange={handleChange} required style={{height:"35px",width:'510px',margin:"10px",backgroundColor:"snow"}}/><br/>
+            <input name="email" placeholder="mail@gmail.com" value={form.email} onChange={handleChange} required style={{height:"35px",width:'400px',marginLeft:"20px",margin:"10px",paddingLeft:"5px",backgroundColor:"snow"}}/><br/>
 
             <label htmlFor="course">Course</label>
-            <input name="course" placeholder="Eg.,IT" value={form.course} onChange={handleChange} required style={{height:"35px",width:'510px',margin:"10px",backgroundColor:"snow"}}/><br/>
+            <input name="course" placeholder="Eg.,IT" value={form.course} onChange={handleChange} required style={{height:"35px",width:'400px',margin:"10px",paddingLeft:"5px",backgroundColor:"snow"}}/><br/>
 
             <label htmlFor="Age">Age</label>
-            <input name="age" type="number" placeholder="Eg.,20" value={form.age} onChange={handleChange} required style={{height:"35px",width:'510px',margin:"10px",backgroundColor:"snow"}}/><br/>
+            <input name="age" type="number" placeholder="Eg.,20" value={form.age} onChange={handleChange} required style={{height:"35px",width:'400px',marginLeft:"30px",margin:"10px",paddingLeft:"5px",backgroundColor:"snow"}}/><br/>
 
             <button type="submit" style={{height:"40px", width:"140px", marginTop:"20px", backgroundColor:"powderblue", borderRadius:"20px"}}>{submitLabel}</button>
         </form>
