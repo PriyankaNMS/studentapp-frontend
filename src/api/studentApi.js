585229767c784fd86a2a77c28bd3bdd615b46810
priyanka <kpriyanka@nmsworks.co.in>
2026-07-21 14:48:29 +0530

    search feature is added.

--- src/api/studentApi.js
+++ src/api/studentApi.js
@@ -7,3 +7,7 @@ export const getStudentById = (id) => axios.get(`${API_URL}/${id}`);
 export const createStudent = (student) => axios.post(`${API_URL}`,student);
 export const updateStudent = (id,student) => axios.put(`${API_URL}/${id}`,student);
 export const deleteStudent = (id) => axios.delete(`${API_URL}/${id}`);
+export const searchStudentByName = (name) => axios.get(`${API_URL}/search/name/${name}`);
+export const searchStudentByCourse=(course) => axios.get(`${API_URL}/search/course/${course}`);
+export const searchStudentByEmail = (email) => axios.get(`${API_URL}/search/email/${email}`);
+export const searchStudentBtAge = (age) => axios.get(`${API_URL}/search/age/${age}`);
--- src/pages/StudentList.js
+++ src/pages/StudentList.js
@@ -1,9 +1,17 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { getAllStudents, deleteStudent } from '../api/studentApi';
+import {
+    getAllStudents,
+    deleteStudent,
+    searchStudentByName,
+    searchStudentByCourse,
+    searchStudentByEmail, searchStudentBtAge
+} from '../api/studentApi';
 
 function StudentList() {
     const [students, setStudents] = useState([]);
+    const [searchField,setSearchField] = useState("name");
+    const [searchTerm,setSearchTerm] = useState("");
 
     useEffect(() => {
         fetchStudents();
@@ -20,13 +28,72 @@ function StudentList() {
         fetchStudents();
     };
 
+    const handleSearch = async () => {
+        if(searchTerm.trim() === ""){
+            fetchStudents();
+            return;
+        }
+        try{
+            let res;
+            switch(searchField){
+                case "name":
+                    res = await searchStudentByName(searchTerm);
+                    break;
+                case "course":
+                    res= await searchStudentByCourse(searchTerm);
+                    break;
+                case "email":
+                    res = await searchStudentByEmail(searchTerm);
+                    break;
+                case "age":
+                    res= await searchStudentBtAge(searchTerm);
+                    break;
+                default:
+                    return;
+            }
+            setStudents(res.data);
+        }
+        catch(error){
+            console.log("Search failed: ",error);
+            setStudents([]);
+        }
+    }
+
     return (
         <div>
             <h1 style={{textAlign:"center"}}>STUDENT MANAGEMENT SYSTEM</h1>
+            <div style={{display:"flex", justifyContent:"space-between"}}>
             <Link to="/students/new">
-                <button style={{height:"30px",width:"300px",backgroundColor:"bisque",marginLeft:"8px",borderRadius:"30px"}}>+ Add Student</button>
+                <button style={{
+                    height:"30px",
+                    width:"300px",
+                    backgroundColor:"bisque",
+                    marginLeft:"8px",
+                    borderRadius:"30px",
+                    border:"0px",
+                    boxShadow:"5px 5px 3px lightgrey"}}>+ Add Student</button>
             </Link>
 
+            <div style={{display:"flex",marginRight:"8px",height:"30px",boxShadow:"5px 5px 3px lightgrey"}}>
+                <select value={searchField}
+                        onChange = {(e)=> setSearchField(e.target.value)}
+                        style={{backgroundColor:"snow", border:"0px", padding:"6px"}}
+                >
+                    <option value = "name">Name</option>
+                    <option value = "course">Course</option>
+                    <option value = "email">Email</option>
+                    <option value = "age">Age</option>
+                </select>
+                <input type = "text"
+                       value={searchTerm}
+                       placeholder="search by field"
+                       onChange={(e)=>setSearchTerm(e.target.value)}
+                       onKeyDown={(e) => e.key === "Enter" && handleSearch()}
+                       style={{backgroundColor:"white", border:"0px"}}
+                />
+                <button onClick={handleSearch} >Search</button>
+            </div>
+            </div>
             <table border="1" cellPadding="8" style={{ marginTop: '20px', width: '100%' }}>
                 <thead style={{backgroundColor:"lavender"}}>
                 <tr>
@@ -43,9 +110,9 @@ function StudentList() {
                         <td>{s.age}</td>
                         <td>
                             <Link to={`/students/${s.id}/edit`}>
-                                <button style={{backgroundColor:"lightyellow", borderRadius:"5px", marginRight:"10px"}}>Edit</button>
+                                <button style={{backgroundColor:"lightyellow", borderRadius:"5px", marginRight:"30px", boxShadow:"2px 2px 3px lightgrey", border:"0px"}}>Edit</button>
                             </Link>
-                            <button onClick={() => handleDelete(s.id)} style={{backgroundColor:"aliceblue", borderRadius:"5px"}}>Delete</button>
+                            <button onClick={() => handleDelete(s.id)} style={{backgroundColor:"aliceblue", borderRadius:"5px", boxShadow:"2px 2px 3px lightgrey", border:"0px"}}>Delete</button>
                         </td>
                     </tr>
                 ))}
